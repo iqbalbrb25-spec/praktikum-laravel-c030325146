afbf602ec23d2e1225ba9a99aeb438756cd141e2
MUHAMMAD IQBAL <iqbal.brb25@gmail.com>
2026-09-17 16:11:51 +0800

    Praktikum 2-4

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Mahasiswa;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Tambahkan rute mahasiswa di sini:
+Route::get('/mahasiswa', function () {
+    $data = Mahasiswa::all();
+    return view('mahasiswa.index', compact('data'));
 });
